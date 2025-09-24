3753da0b7435f47595a69cae778cda12ea0efce3
USUARIO <USUARIO@CAVALLINI>
2025-09-23 19:55:09 -0600

    Agregar archivos de proyecto.

---
+++ Default.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class _Default
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        Dim p1 As New Persona()
+        Dim p2 As New Persona(25)
+    End Sub
+End Class
